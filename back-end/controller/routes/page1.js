0ad6039854c72f8d3b0ddaefcffbe1ea9f281906
Kabir_Sethi <Kabirsethione@gmail.com>
2020-06-04 17:10:20 +0530

    Changed schema and optimesed all the route handlers

--- back-end/controller/routes/page1.js
+++ back-end/controller/routes/page1.js
@@ -4,39 +4,64 @@ const router = express.Router();
 const teams = require('../../model/cricketTeamMod'); 
 var selectedPlrs = [];
 var callplrs = []
-var stringify = require('json-stringify-safe');
 
-router.get('/team/getTeam/:tm', (req,res,next)=>{// player[name].
-    teams.find({'teamName': req.params.tm},(err,data)=>{
+router.get('/team/getTeam/:teamId', (req,res,next)=>{// player[name].
+    teams.findById(req.params.teamId,(err,data)=>{
         if(err) throw err;
         callplrs = [];
-        data.map((item)=>{
-            callplrs.push({'name':item.player.name})});
-        //     callplrs.push(item)});
+        data.players.map((item)=>{
+            callplrs.push({'name':item.name})});
+        // res.send(data);
         res.send({team:callplrs});
     });
 });
 
 router.put('/team/update', (req, res, next)=>{
     var selectedPlrs = req.body.team;
-    selectedPlrs.forEach((item)=>{
-        var filter = {'player.name': item.name};
-        var update = {'player.active' : 'true'}
-        
-        teams.findOneAndUpdate(filter, update, {new: true, useFindAndModify: false }, (err, data) => {
-            console.log(data);
-        });
-    });
+   selectedPlrs.map((item)=>{
 
+    let filter = {players: {$elemMatch:{name: item.name}}}
+    let update = {"players.$.active":'false'}
+    teams.findOneAndUpdate(filter,update,(err,data)=>{
+        res.send(data);
+    })
+   }) 
 });
 
 //!! Only for back-end to initialise the team players through POSTMAN
-router.post('/team/postTeam',(req,res,next)=>{
+function addPlayer(team,data){
+
+    let filter = {'teamName': team} 
+    let update1 = {$addToSet :{players:{$each: data}} };
+    let update2 = {$addToSet :{players:  data[0]} };
+    if(data.length>1){
+        teams.updateOne(filter,update1 ,(err, dat)=>{
+            if (err) throw err;
+            console.log(data);
+            return dat;
+        })
+        // console.log(data)
+    }else {
+        teams.updateOne(filter,update2, {new: true },(err, dat)=>{
+        if (err) throw err;
+        console.log(data[0]);
+        return dat; });
+    }    
+}
+
+router.post('/team/createTeam',(req,res,next)=>{
     console.log("Recieved: "+req.body);
     // teams.save(req.body).then((data)=>{ // returns a promise, create is a mongooose method
-    teams.create(req.body).then((data)=>{
-        res.send(data)});
-    // });
+    teams.findOne({'teamName': req.body.teamName},(err,data)=>{
+        if(data){
+            console.log("Team exists already");
+            res.send(addPlayer(req.body.teamName, req.body.players));
+        }else {
+            console.log("Building new team");
+            teams.create(req.body).then((data)=>{
+            res.send(data)});             
+        }    
+    });
 });
 
 
--- back-end/model/cricketTeamMod.js
+++ back-end/model/cricketTeamMod.js
@@ -2,45 +2,29 @@ const mongoose = require('mongoose');
 mongoose.set('debug',true);
 
 const Schema = mongoose.Schema;
-// const playerSchema = new Schema([
-//     {
-//         name:{
-//             type:String,
-//             required: [true,'name field is required']
-//         },
-//         position:String,
-//         scored:{
-//             type:Number,
-//             default: 0
-//         },
-//         runs:Number,
-//         active:Boolean
-//     }
-// ]);
-
-const cricSchema = new Schema({
- 
-        teamName:{
+const playerSchema = new Schema(
+    {
+        name:{
             type:String,
-            required: [true,'teamName field is required!']
+            required: [true,'name field is required']
         },
-        player:
-            {
-                name:{
-                    type:String,
-                    required: [true,'name field is required']
-                },
-                position:String,
-                scored:{
-                    type:Number,
-                    default: 0
-                },
-                runs:Number,
-                active:Boolean
-            }
-        
+        position:String,
+        scored:{
+            type:Number,
+            default: 0
+        },
+        runs:Number,
+        active:Boolean
     }
-);   
+);
+
+const cricSchema = new Schema({ 
+     teamName:{
+        type:String,
+        required: [true,'teamName field is required!']
+    },
+    players: [playerSchema]
+});   
 
 const model = mongoose.model('cricketTeamCol', cricSchema);
 
